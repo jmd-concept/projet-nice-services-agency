f819156efc0fbccc08ad5ea31a7f79048f6ac3d9
jmd-concept <jmdconcept3@gmail.com>
2026-06-26 22:41:29 +0100

    1.3.1 Erreur serveur component app/contact/page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,27 @@
-
+'use client'
 import ContactForm from "@/components/ContactForm";
 
-export default function ContactFormPage(){
-    return <ContactForm />
+/**
+ * "use client";
+
+import { useState } from "react";
+import ContactForm from "./ContactForm";
+
+export default function ContactFormPage() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const togglePanel = () => setIsOpen(false);
+
+  return (
+    <>
+      {isOpen && <ContactForm togglePanel={togglePanel} />}
+    </>
+  );
 }
+
+ */
+
+export default function ContactFormPage(){
+    return <ContactForm togglePanel={() => {}} />
+}
+  
